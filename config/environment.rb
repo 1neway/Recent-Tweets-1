a7001060547d16b35497da7dc89dd2d2243607a3
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-29 09:55:38 -0500

    Twitter
    setting env variables and tokens, secrets

--- config/environment.rb
+++ config/environment.rb
@@ -32,3 +32,10 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+Twitter.configure do |config|
+  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+  config.oauth_token = ENV["TWITTER_OAUTH_TOKEN"]
+  config.oauth_token_secret = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
+end
